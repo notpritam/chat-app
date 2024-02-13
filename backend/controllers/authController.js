62d2638fc211b9d1b6aaa2b839902624540abca8
Pritam Sharma <notpritamsharma@gmail.com>
2024-02-13 23:50:22 +0530

    Register Complete

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,8 +3,17 @@ import authService from "../services/authService.js";
 const authController = {
   register: async (req, res) => {
     try {
-      const { username, password } = req.body;
-      const user = await authService.register(username, password);
+      const { username, password, image, name } = req.body;
+      const user = await authService.register({
+        username,
+        password,
+        image,
+        name,
+      });
+
+      const token = authService.generateToken(user);
+
+      res.status(201).json({ user, token });
     } catch (err) {
       console.log(err);
       res.status(401).json({ message: err.message });
--- backend/services/authService.js
+++ backend/services/authService.js
@@ -3,12 +3,13 @@ import User from "../models/User.js";
 
 const authService = {
   login: async (username, password) => {},
-  register: async (username, image, password) => {
+  register: async ({ username, image, password, name }) => {
     try {
       const user = new User({
         username,
         image,
         password,
+        name,
       });
       await user.save();
       return user;
@@ -16,6 +17,11 @@ const authService = {
       console.log(err);
       res.status(401).json({ message: err.message });
     }
+  },
+  generateToken: (user) => {
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
   },
 };
 
