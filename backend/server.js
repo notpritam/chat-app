effad9db9d931f3177c2754b712b10ae62d486fe
Pritam Sharma <notpritamsharma@gmail.com>
2024-02-14 01:34:07 +0530

    Send Message in Room

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,14 @@
 import app from "./app.js";
+import socketIo from "socket.io";
+
+const server = http.createServer(app);
+export const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  // Handle socket events here
+});
 
 const PORT = process.env.PORT || 3001;
 
--- backend/services/roomService.js
+++ backend/services/roomService.js
@@ -1,4 +1,5 @@
 import Room from "../models/Room.js";
+import { io } from "../server.js";
 
 const roomService = {
   getRooms: async ({ user }) => {
@@ -37,6 +38,25 @@ const roomService = {
         });
         await newRoom.save();
         return newRoom;
+      }
+    } catch (err) {
+      return err;
+    }
+  },
+  sendMessageinRoom: async ({ room, message, user }) => {
+    try {
+      const existingRoom = await Room.findOne({ name: room.name });
+      if (!existingRoom) {
+        return new Error("Room not found");
+      } else if (existingRoom.members.includes(user._id)) {
+        existingRoom.messages.push(message._id);
+        await existingRoom.save();
+
+        io.to(existingRoom.name).emit("message", message);
+
+        return existingRoom;
+      } else {
+        return new Error("You are not a member of this room");
       }
     } catch (err) {
       return err;
